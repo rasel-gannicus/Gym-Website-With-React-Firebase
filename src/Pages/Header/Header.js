45517b140773e4ae7fb25ec69e49163da790bd86
shafik720 <shafikrasel5@gmail.com>
2023-04-25 17:52:12 +0600

    creating banner

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div className="flex justify-between border-2 border-rose-600 mt-10 px-7 py-4 w-4/5 mx-auto rounded-md text-xl font-medium">
             <div className="">
                 <h1>Fitness</h1>
             </div>
             <div className="">
-                <a href="">Home</a>
-                <a href="">About</a>
-                <a href="">Trainers</a>
-                <a href="">Blog</a>
-                <a href="">Contact</a>
+                <a className="mr-4 hover:text-sky-600" href="">Home</a>
+                <a className="mr-4 hover:text-sky-600" href="">About</a>
+                <a className="mr-4 hover:text-sky-600" href="">Trainers</a>
+                <a className="mr-4 hover:text-sky-600" href="">Blog</a>
+                <a className="mr-4 hover:text-sky-600" href="">Contact</a>
             </div>
         </div>
     );
