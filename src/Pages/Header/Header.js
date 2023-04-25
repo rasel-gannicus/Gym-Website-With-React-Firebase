04888826fbd7baf382dca9189ac6f1facebbda53
shafik720 <shafikrasel5@gmail.com>
2023-04-25 17:06:24 +0600

    setting up

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="">
+                <h1>Fitness</h1>
+            </div>
+            <div className="">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Trainers</a>
+                <a href="">Blog</a>
+                <a href="">Contact</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
