899b67edf9f8cc2fbf1666ad2615452b2ec00892
shafik720 <shafikrasel5@gmail.com>
2023-04-27 00:23:58 +0600

    banner creating

--- src/Pages/Header/Banner/Banner.js
+++ src/Pages/Header/Banner/Banner.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./Banner.css";
+import fbIcon from "../../../assets/img/facebook.png";
+import twitterIcon from "../../../assets/img/twitter.png";
+import instaIcon from "../../../assets/img/instagram.png";
 
 const Banner = () => {
     return (
@@ -7,6 +10,12 @@ const Banner = () => {
             <h1>Build Perfect Body <br/> With Clean Mind </h1>
             <p>Looking to achieve the perfect body? At our gym, we believe that a clean mind is just as important as a strong physique. Our state-of-the-art facilities and expert trainers will help you reach your fitness goals while promoting mental clarity and well-being. Join us today and start building the body and mind you've always wanted.</p>
             <button>Get Started</button>
+
+            <div className="social-button">
+                <img src={fbIcon} alt=""/>
+                <img src={twitterIcon} alt=""/>
+                <img src={instaIcon} alt=""/>
+            </div>
         </div>
     );
 };
