bbf32fc160ffff870a0c61d7e579438431ffb4c4
shafik720 <shafikrasel5@gmail.com>
2023-04-26 23:49:22 +0600

    banner creating

---
+++ src/Pages/Header/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="banner-details">
+            <h1>Build Perfect Body <br/> With Clean Mind </h1>
+            <p>Looking to achieve the perfect body? At our gym, we believe that a clean mind is just as important as a strong physique. Our state-of-the-art facilities and expert trainers will help you reach your fitness goals while promoting mental clarity and well-being. Join us today and start building the body and mind you've always wanted.</p>
+            <button>Get Started</button>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import Banner from './Banner/Banner';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div className="flex justify-between border-2 border-rose-600 mt-10 px-7 py-4 w-4/5 mx-auto rounded-md text-xl font-medium">
-            <div className="">
-                <h1>Fitness</h1>
+        <div className="full-header">
+            <div className="header-nav">
+                <div className="header-left">
+                    <h1>Fitness</h1>
+                </div>
+                <div className="header-right">
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Trainers</a>
+                    <a href="">Blog</a>
+                    <a href="">Contact</a>
+                </div>
             </div>
-            <div className="">
-                <a className="mr-4 hover:text-sky-600" href="">Home</a>
-                <a className="mr-4 hover:text-sky-600" href="">About</a>
-                <a className="mr-4 hover:text-sky-600" href="">Trainers</a>
-                <a className="mr-4 hover:text-sky-600" href="">Blog</a>
-                <a className="mr-4 hover:text-sky-600" href="">Contact</a>
-            </div>
+            <Banner></Banner>
         </div>
     );
 };
