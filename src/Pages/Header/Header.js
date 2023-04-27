ccfdcdde63935befffa1a418914f48cae2b540b8
shafik720 <shafikrasel5@gmail.com>
2023-04-27 20:42:03 +0600

    creating menu for mobile verson

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -9,12 +9,16 @@ const Header = () => {
     const displayMenu = () => {
         const menuOne = document.querySelector('.menuOne');
         const menuTwo = document.querySelector('.menuTwo');
+        const headerMenu = document.querySelector('.header-right');
         menuOne.classList.add('active');
         menuTwo.classList.add('active');
+        headerMenu.classList.add('active');
     }
     const hideMenu = () => {
         const menuOne = document.querySelector('.menuOne');
         const menuTwo = document.querySelector('.menuTwo');
+        const headerMenu = document.querySelector('.header-right');
+        headerMenu.classList.remove('active');
         menuOne.classList.remove('active');
         menuTwo.classList.remove('active');
     }
@@ -24,16 +28,18 @@ const Header = () => {
                 <div className="header-left">
                     <h1>Fitness</h1>
                 </div>
-                 <div className="header-right">
-                    <a href="">Home</a>
-                    <a href="">About</a>
-                    <a href="">Trainers</a>
-                    <a href="">Blog</a>
-                    <a href="">Contact</a>
+                <div className="header-right">
+                    <div className="header-right-menu">
+                        <a href="">Home</a>
+                        <a href="">About</a>
+                        <a href="">Trainers</a>
+                        <a href="">Blog</a>
+                        <a href="">Contact</a>
+                    </div>
                 </div>
                 <div className="menu-icon">
-                    <img  onClick={displayMenu}  className="menuOne" src={menuOne} alt=""/>
-                    <img onClick={hideMenu}  className="menuTwo" src={menuTwo} alt=""/>
+                    <img onClick={displayMenu} className="menuOne" src={menuOne} alt="" />
+                    <img onClick={hideMenu} className="menuTwo" src={menuTwo} alt="" />
                 </div>
             </div>
             <Banner></Banner>
