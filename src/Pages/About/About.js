3242f6b3df796c4a4d1e3b73d28a377080f394b7
shafik720 <shafikrasel5@gmail.com>
2023-04-28 15:33:06 +0600

    about section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,6 +8,7 @@ const About = () => {
             <div className="about-header">
                 <p>About Our Company</p>
                 <h1>Who We Are and What We Do </h1>
+                <hr/>
             </div>  
             <div className="about-mains">
                 <div className="about-main">
@@ -20,11 +21,55 @@ const About = () => {
                             <p>Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he decided to focus on arriving.</p>
                         </div>
                     </div>
-                    <div className="about-single"></div>
-                    <div className="about-single"></div>
+                    <div className="about-single">
+                        <div className="about-single-header">
+                            <h2>Modern Equipment</h2>
+                            <img src={equip1} alt=""/>
+                        </div>
+                        <div className="about-single-details">
+                            <p>Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he decided to focus on arriving.</p>
+                        </div>
+                    </div>
+                    <div className="about-single">
+                        <div className="about-single-header">
+                            <h2>Modern Equipment</h2>
+                            <img src={equip1} alt=""/>
+                        </div>
+                        <div className="about-single-details">
+                            <p>Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he decided to focus on arriving.</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="about-main"></div>
-                <div className="about-main"></div>
+                <div className="about-main">
+                    <div className="about-single">
+                        <div className="about-single-header">
+                            <h2>Modern Equipment</h2>
+                            <img src={equip1} alt=""/>
+                        </div>
+                        <div className="about-single-details">
+                            <p>Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he decided to focus on arriving.</p>
+                        </div>
+                    </div>
+                    <div className="about-single">
+                        <div className="about-single-header">
+                            <h2>Modern Equipment</h2>
+                            <img src={equip1} alt=""/>
+                        </div>
+                        <div className="about-single-details">
+                            <p>Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he decided to focus on arriving.</p>
+                        </div>
+                    </div>
+                    <div className="about-single">
+                        <div className="about-single-header">
+                            <h2>Modern Equipment</h2>
+                            <img src={equip1} alt=""/>
+                        </div>
+                        <div className="about-single-details">
+                            <p>Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he decided to focus on arriving.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
