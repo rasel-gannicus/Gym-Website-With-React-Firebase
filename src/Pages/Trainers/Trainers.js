8c205c6d6e44f79abe98c5345c75336b1eaa7573
shafik720 <shafikrasel5@gmail.com>
2023-04-28 20:01:11 +0600

    trainer section styling

--- src/Pages/Trainers/Trainers.js
+++ src/Pages/Trainers/Trainers.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import './Trainers.css';
 import trainer1 from "../../assets/img/02.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons"
+
+
 
 const Trainers = () => {
     return (
         <div className="trainer-div">
             <div className="trainer-header">
                 <h4>Train With Expert</h4>
-                <h1>Meet The magnificent Trainers</h1>
-                <hr/>
+                <h1>Meet The Magnificent Trainers</h1>
+                <hr />
             </div>
             <div className="trainer-details">
                 <div className="trainer-card">
                     <div className="trainer-pic">
-                        <img src={trainer1} alt=""/>
+                        <img src={trainer1} alt="" />
                     </div>
                     <div className="trainer-info">
                         <h3>Abir Rahman</h3>
                         <p>Cardio Expert</p>
+                    </div>
+                    <div className="trainer-social">
+                        <div className="trainer-social-icons">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </div>
+                        <div className="trainer-social-icons">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </div>
+                        <div className="trainer-social-icons">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </div>
+                        <div className="trainer-social-icons">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </div>
                     </div>
                 </div>
             </div>
