baa5fd8e83f71423e72957e7dd78b4c106e722b8
shafik720 <shafikrasel5@gmail.com>
2023-04-28 19:22:53 +0600

    trainer section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Pages/Header/Header';
 import Features from './Pages/Features/Features';
 import About from './Pages/About/About';
+import Trainers from './Pages/Trainers/Trainers';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Features></Features>
       <About></About>
+      <Trainers></Trainers>
 
 
       <h1 style={{marginTop : '300px'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, natus tenetur esse dignissimos eligendi voluptatum porro! Maiores non, reprehenderit optio aspernatur illum tenetur, facere molestias, ex mollitia nesciunt perspiciatis architecto saepe ratione aut ipsam numquam enim voluptatibus voluptates aperiam maxime doloribus nam atque laudantium unde? Eum repudiandae voluptatum ab quae.</h1>
---
+++ src/Pages/Trainers/Trainers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Trainers.css';
+
+const Trainers = () => {
+    return (
+        <div className="trainer-div">
+            <div className="trainer-header">
+                <h4>Train With Expert</h4>
+                <h1>Meet The magnificent Trainers</h1>
+                <hr/>
+            </div>
+        </div>
+    );
+};
+
+export default Trainers;
