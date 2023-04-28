407632b3c598ddf40363e563ccbb15556904aea7
shafik720 <shafikrasel5@gmail.com>
2023-04-28 19:37:27 +0600

    trainer section

--- src/Pages/Trainers/Trainers.js
+++ src/Pages/Trainers/Trainers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Trainers.css';
+import trainer1 from "../../assets/img/02.jpg";
 
 const Trainers = () => {
     return (
@@ -8,6 +9,17 @@ const Trainers = () => {
                 <h4>Train With Expert</h4>
                 <h1>Meet The magnificent Trainers</h1>
                 <hr/>
+            </div>
+            <div className="trainer-details">
+                <div className="trainer-card">
+                    <div className="trainer-pic">
+                        <img src={trainer1} alt=""/>
+                    </div>
+                    <div className="trainer-info">
+                        <h3>Abir Rahman</h3>
+                        <p>Cardio Expert</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
