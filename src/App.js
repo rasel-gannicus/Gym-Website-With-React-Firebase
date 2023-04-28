27f8db5e68ba7a10c0f9df61e16b9b1b9fa02352
shafik720 <shafikrasel5@gmail.com>
2023-04-28 15:21:17 +0600

    working on about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Pages/Header/Header';
 import Features from './Pages/Features/Features';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Features></Features>
+      <About></About>
 
 
       <h1 style={{marginTop : '300px'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, natus tenetur esse dignissimos eligendi voluptatum porro! Maiores non, reprehenderit optio aspernatur illum tenetur, facere molestias, ex mollitia nesciunt perspiciatis architecto saepe ratione aut ipsam numquam enim voluptatibus voluptates aperiam maxime doloribus nam atque laudantium unde? Eum repudiandae voluptatum ab quae.</h1>
---
+++ src/Pages/About/About.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './About.css';
+import equip1 from '../../assets/img/cycling.png';
+
+const About = () => {
+    return (
+        <div className="about-section" >
+            <div className="about-header">
+                <p>About Our Company</p>
+                <h1>Who We Are and What We Do </h1>
+            </div>  
+            <div className="about-mains">
+                <div className="about-main">
+                    <div className="about-single">
+                        <div className="about-single-header">
+                            <h2>Modern Equipment</h2>
+                            <img src={equip1} alt=""/>
+                        </div>
+                        <div className="about-single-details">
+                            <p>Benjamin Franklin, inventor, statesman, writer, publisher and economist relates in his autobiography that early in his life he decided to focus on arriving.</p>
+                        </div>
+                    </div>
+                    <div className="about-single"></div>
+                    <div className="about-single"></div>
+                </div>
+                <div className="about-main"></div>
+                <div className="about-main"></div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
