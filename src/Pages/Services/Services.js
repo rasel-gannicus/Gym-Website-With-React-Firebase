79f3c975b022aa8cf47022cf7c2866cf66f9bb4f
shafik720 <shafikrasel5@gmail.com>
2023-04-29 18:00:02 +0600

    service section almost done

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -21,128 +21,92 @@ const Services = () => {
                     <div className="service-img">
                         <img src={service5} alt="" />
                         <div className="special-icons">
-                            <div className="special-icon1">
-                                $ 150
-                            </div>
-                            <div className="special-icon2">
-                                3 Months
-                            </div>
-                            <div className="special-icon3">
-                                7hrs/week
-                            </div>
+                            <p>$ 150</p>
+                            <p>3 Months</p>
+                            <p>7 hrs/week</p>
+                        </div>
+                    </div>
+                    <div className="card-details">
+                        <h2>Fitness For Beginners</h2>
+                        <p>Make a list of your achievements toward your long-term goal and remind.</p>
+                    </div>
+                    <button>Enroll Now</button>
+                </div>
+                <div className="service-card">
+                    <div className="service-img">
+                        <img src={service6} alt="" />
+                        <div className="special-icons">
+                            <p>$ 250</p>
+                            <p>4 Months</p>
+                            <p>9 hrs/week</p>
                         </div>
                     </div>
                     <div className="card-details">
                         <h2>Private & Group Lessons</h2>
                         <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
                     </div>
-                    <button>Purchase Plan</button>
+                    <button>Enroll Now</button>
                 </div>
                 <div className="service-card">
                     <div className="service-img">
-                        <img src={service5} alt="" />
+                        <img src={service1} alt="" />
                         <div className="special-icons">
-                            <div className="special-icon1">
-                                $ 150
-                            </div>
-                            <div className="special-icon2">
-                                3 Months
-                            </div>
-                            <div className="special-icon3">
-                                7hrs/week
-                            </div>
+                            <p>$ 300</p>
+                            <p>6 Months</p>
+                            <p>9 hrs/week</p>
                         </div>
                     </div>
                     <div className="card-details">
-                        <h2>Private & Group Lessons</h2>
+                        <h2>Arm Improvement</h2>
                         <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
                     </div>
-                    <button>Purchase Plan</button>
+                    <button>Enroll Now</button>
                 </div>
                 <div className="service-card">
                     <div className="service-img">
-                        <img src={service5} alt="" />
+                        <img src={service2} alt="" />
                         <div className="special-icons">
-                            <div className="special-icon1">
-                                $ 150
-                            </div>
-                            <div className="special-icon2">
-                                3 Months
-                            </div>
-                            <div className="special-icon3">
-                                7hrs/week
-                            </div>
+                            <p>$ 350</p>
+                            <p>2 Months</p>
+                            <p>7 hrs/week</p>
                         </div>
                     </div>
                     <div className="card-details">
-                        <h2>Private & Group Lessons</h2>
-                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                        <h2>Yoga Charming</h2>
+                        <p>Let success motivate you. Find a picture of what epitomizes success to you.</p>
                     </div>
-                    <button>Purchase Plan</button>
+                    <button>Enroll Now</button>
                 </div>
                 <div className="service-card">
                     <div className="service-img">
-                        <img src={service5} alt="" />
+                        <img src={service3} alt="" />
                         <div className="special-icons">
-                            <div className="special-icon1">
-                                $ 150
-                            </div>
-                            <div className="special-icon2">
-                                3 Months
-                            </div>
-                            <div className="special-icon3">
-                                7hrs/week
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-details">
-                        <h2>Private & Group Lessons</h2>
-                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
-                    </div>
-                    <button>Purchase Plan</button>
-                </div>
-                <div className="service-card">
-                    <div className="service-img">
-                        <img src={service5} alt="" />
-                        <div className="special-icons">
-                            <div className="special-icon1">
-                                $ 150
-                            </div>
-                            <div className="special-icon2">
-                                3 Months
-                            </div>
-                            <div className="special-icon3">
-                                7hrs/week
-                            </div>
+                            <p>$ 450</p>
+                            <p>4 Months</p>
+                            <p>8 hrs/week</p>
                         </div>
                     </div>
 
                     <div className="card-details">
-                        <h2>Private & Group Lessons</h2>
-                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                        <h2>Fitness Core</h2>
+                        <p>Reflect and experiment until you find the right combination of motivators for.</p>
                     </div>
-                    <button>Purchase Plan</button>
+                    <button>Enroll Now</button>
                 </div>
                 <div className="service-card">
                     <div className="service-img">
-                        <img src={service5} alt="" />
+                        <img src={service4} alt="" />
                         <div className="special-icons">
-                            <div className="special-icon1">
-                                $ 150
-                            </div>
-                            <div className="special-icon2">
-                                3 Months
-                            </div>
-                            <div className="special-icon3">
-                                7hrs/week
-                            </div>
+                            <p>$ 1150</p>
+                            <p>9 Months</p>
+                            <p>12 hrs/week</p>
                         </div>
                     </div>
                     <div className="card-details">
-                        <h2>Private & Group Lessons</h2>
-                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                        <h2>Fitness Restore</h2>
+                        <p>Success isn’t really that difficult. There is a significant portion of the population.</p>
                     </div>
-                    <button>Purchase Plan</button>
+                    <button>Enroll Now</button>
                 </div>
             </div>
         </div>
