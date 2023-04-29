06615111bc17b50f88e3ad3dc30c5b1239c9d05c
shafik720 <shafikrasel5@gmail.com>
2023-04-29 15:02:48 +0600

    service section

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Services.css';
+
 import service1 from '../../assets/img/Services/services (1).jpg';
+import service2 from '../../assets/img/Services/services (2).jpg';
+import service3 from '../../assets/img/Services/services (7).jpg';
+import service4 from '../../assets/img/Services/services (4).jpg';
+import service5 from '../../assets/img/Services/services (5).jpg';
+import service6 from '../../assets/img/Services/services (6).jpg';
 
 const Services = () => {
     return (
@@ -13,6 +19,91 @@ const Services = () => {
             <div className="service-details">
                 <div className="service-card">
                     <img src={service1} alt=""/>
+                    <div className="special-icon1">
+                        $ 150
+                    </div>
+                    <div className="special-icon2">
+                        3 Months
+                    </div>
+                    <div className="special-icon3">
+                        7hrs/week
+                    </div>
+                    <div className="card-details">
+                        <h2>Private & Group Lessons</h2>
+                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                    </div>
+                    <button>Purchase Plan</button>
+                </div>
+                <div className="service-card">
+                    <img src={service2} alt=""/>
+                    <div className="special-icon1">
+                        $ 150
+                    </div>
+                    <div className="special-icon2">
+                        3 Months
+                    </div>
+                    <div className="special-icon3">
+                        7hrs/week
+                    </div>
+                    <div className="card-details">
+                        <h2>Private & Group Lessons</h2>
+                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                    </div>
+                    <button>Purchase Plan</button>
+                </div>
+                <div className="service-card">
+                    <img src={service3} alt=""/>
+                    <div className="special-icon1">
+                        $ 150
+                    </div>
+                    <div className="special-icon2">
+                        3 Months
+                    </div>
+                    <div className="special-icon3">
+                        7hrs/week
+                    </div>
+                    <div className="card-details">
+                        <h2>Private & Group Lessons</h2>
+                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                    </div>
+                    <button>Purchase Plan</button>
+                </div>
+                <div className="service-card">
+                    <img src={service4} alt=""/>
+                    <div className="special-icon1">
+                        $ 150
+                    </div>
+                    <div className="special-icon2">
+                        3 Months
+                    </div>
+                    <div className="special-icon3">
+                        7hrs/week
+                    </div>
+                    <div className="card-details">
+                        <h2>Private & Group Lessons</h2>
+                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                    </div>
+                    <button>Purchase Plan</button>
+                </div>
+                <div className="service-card">
+                    <img src={service5} alt=""/>
+                    <div className="special-icon1">
+                        $ 150
+                    </div>
+                    <div className="special-icon2">
+                        3 Months
+                    </div>
+                    <div className="special-icon3">
+                        7hrs/week
+                    </div>
+                    <div className="card-details">
+                        <h2>Private & Group Lessons</h2>
+                        <p>Give yourself the power of responsibility the only thing stopping you is yourself.</p>
+                    </div>
+                    <button>Purchase Plan</button>
+                </div>
+                <div className="service-card">
+                    <img src={service6} alt=""/>
                     <div className="special-icon1">
                         $ 150
                     </div>
