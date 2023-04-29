a5dede64f583a9ceea40c43a838951c535e46e9b
shafik720 <shafikrasel5@gmail.com>
2023-04-29 22:18:54 +0600

    login page styling done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,8 +3,25 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login Here</h2>
+        <div className="login-div">
+            <h2>Login</h2>
+            <hr />
+            <div className="login-form">
+                <form action="">
+                    <div className="">
+                        <input type="email" name="" placeholder="Your Email" id="" />
+                    </div>
+                    <div className="">
+                        <input type="password" name="" placeholder="Your Password" id="" />
+
+                        <button>Login</button>
+                    </div>
+                </form>
+            </div>
+            <div className="forgot-link">
+                <span><a href="">Forgot Password / &nbsp; </a></span>
+                <span><a href=""> Register</a></span>
+            </div>
         </div>
     );
 };
