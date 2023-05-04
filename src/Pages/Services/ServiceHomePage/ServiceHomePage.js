267db1d46930d1c735320431a11b905cc6342502
shafik720 <shafikrasel5@gmail.com>
2023-05-04 22:19:30 +0600

    style on service fixed

--- src/Pages/Services/ServiceHomePage/ServiceHomePage.js
+++ src/Pages/Services/ServiceHomePage/ServiceHomePage.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 
-const ServiceHomePage = ({data}) => {
-    
-    const {serviceId,plan,serviceName,short_desc,img,basic_plan} = data ;
-    
+const ServiceHomePage = ({ data }) => {
+
+    const { serviceId, plan, serviceName, short_desc, img, basic_plan } = data;
+
     // --- navigating to individual service detail page when user will click see more button
     const navigate = useNavigate();
-    const servicePage= () => {
+    const servicePage = () => {
         navigate('/serviceDetails');
     }
     return (
-            <div className="service-card">
-                    <div className="service-img">
-                        <img src={img} alt="" />
-                        <div className="special-icons">
-                            <p>$ {basic_plan.price}</p>
-                            <p>{basic_plan.duration} Months</p>
-                            <p>{basic_plan.hrsPerWeek} hrs/week</p>
-                        </div>
+        <div className="service-card">
+            <div className="">
+                <div className="service-img">
+                    <img src={img} alt="" />
+                    <div className="special-icons">
+                        <p>$ {basic_plan.price}</p>
+                        <p>{basic_plan.duration} Months</p>
+                        <p>{basic_plan.hrsPerWeek} hrs/week</p>
                     </div>
-                    <div className="card-details">
-                        <h2>{serviceName}</h2>
-                        <p>{short_desc}</p>
-                    </div>
-                    <button onClick={servicePage}>See Details</button>
                 </div>
+                <div className="card-details">
+                    <h2>{serviceName}</h2>
+                    <p>{short_desc}</p>
+                </div>
+            </div>
+            <button onClick={servicePage}>See Details</button>
+        </div>
     );
 };
 
