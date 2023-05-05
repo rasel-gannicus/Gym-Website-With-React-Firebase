1fc948b99aa81974103d0c5e4a02d022b5111b82
shafik720 <shafikrasel5@gmail.com>
2023-05-05 11:58:32 +0600

    making single card

--- src/Pages/Services/ServiceDetails/ServiceDetailCard.js
+++ src/Pages/Services/ServiceDetails/ServiceDetailCard.js
@@ -5,47 +5,44 @@ import './ServiceDetailCard.css';
 import check from '../../../assets/img/check.png';
 import close from '../../../assets/img/close.png';
 
-const ServiceDetailCard = ({service}) => {
-    const {Aerobics, cardio, crossfit, plan, price,shortDesc,snacks,swimmingPool,vipRestroom} = service ;
-    console.log(plan);
+const ServiceDetailCard = ({ service }) => {
+    const { Aerobics, cardio, crossfit, plan, price, shortDesc, snacks, swimmingPool, vipRestroom } = service;
     return (
-        <div className="detail-cards">
-            <div className="detail-card-single">
-                <div className="detail-card-header">
-                    <h2>{plan}</h2>
-                    <hr className="horizontal" />
-                    <p>Positive pleasure-oriented goals are much more powerful.</p>
-                    <h1>$ 12<span>/Month</span> </h1>
+        <div className="detail-card-single">
+            <div className="detail-card-header">
+                <h2>{plan}</h2>
+                <hr className="horizontal" />
+                <p>Positive pleasure-oriented goals are much more powerful.</p>
+                <h1>$ 12<span>/Month</span> </h1>
+            </div>
+            <div className="detail-card-details">
+                <div className="">
+                    <p>Cardio</p>
+                    <span><img src={check} alt="" /></span>
                 </div>
-                <div className="detail-card-details">
-                    <div className="">
-                        <p>Cardio</p>
-                        <span><img src={check} alt="" /></span>
-                    </div>
-                    <div className="">
-                        <p>Swimming Pool</p>
-                        <span><img src={close} alt="" /></span>
-                    </div>
-                    <div className="">
-                        <p>Snacks</p>
-                        <span><img src={close} alt="" /></span>
-                    </div>
-                    <div className="">
-                        <p>Vip Restroom</p>
-                        <span><img src={close} alt="" /></span>
-                    </div>
-                    <div className="">
-                        <p>Aerobics</p>
-                        <span><img src={check} alt="" /></span>
-                    </div>
-                    <div className="">
-                        <p>Crossfit</p>
-                        <span><img src={check} alt="" /></span>
-                    </div>
+                <div className="">
+                    <p>Swimming Pool</p>
+                    <span><img src={close} alt="" /></span>
                 </div>
-                <div className="service-button-div">
-                    <button>Buy Membership</button>
+                <div className="">
+                    <p>Snacks</p>
+                    <span><img src={close} alt="" /></span>
                 </div>
+                <div className="">
+                    <p>Vip Restroom</p>
+                    <span><img src={close} alt="" /></span>
+                </div>
+                <div className="">
+                    <p>Aerobics</p>
+                    <span><img src={check} alt="" /></span>
+                </div>
+                <div className="">
+                    <p>Crossfit</p>
+                    <span><img src={check} alt="" /></span>
+                </div>
+            </div>
+            <div className="service-button-div">
+                <button>Buy Membership</button>
             </div>
         </div>
     );
--- src/Pages/Services/ServiceDetails/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails/ServiceDetails.js
@@ -7,29 +7,32 @@ import ClipLoader from 'react-spinners/ClipLoader';
 const ServiceDetails = () => {
     const params = useParams();
 
-     // --- Loading service detail data from redux toolkit api
-     const{data, isLoading, isError, error} = useGetGymServicesQuery();
-    
-     // --- deciding what to print while loading data 
-     let content = null ; 
-     if(isLoading && !isError){
-         content = <div className="custom-loader"><ClipLoader color="black" size={75} /></div>
-     } 
-     if(!isLoading && isError){
-         console.log(error);
-         content = <div className="">There was an error loading the content</div>
-     }
-     let content2 = null ; 
-     if(!isLoading && !isError && data.length>0){
+    // --- Loading service detail data from redux toolkit api
+    const { data, isLoading, isError, error } = useGetGymServicesQuery();
+
+    // --- deciding what to print while loading data 
+    let content = null;
+    if (isLoading && !isError) {
+        content = <div className="custom-loader"><ClipLoader color="black" size={75} /></div>
+    }
+    if (!isLoading && isError) {
+        console.log(error);
+        content = <div className="">There was an error loading the content</div>
+    }
+    let content2 = null;
+    if (!isLoading && !isError && data.length > 0) {
         content2 = data[params.id].allPlan;
-         console.log(content2);
-         content = content2.map(index=><ServiceDetailCard service={index} ></ServiceDetailCard>)
-     }
-    
+        console.log(content2);
+        content = content2.map(index => <ServiceDetailCard service={index} key={index.plan} ></ServiceDetailCard>)
+    }
+
     return (
         <div className="service-card-details">
-            <h2>Service Details here</h2>
-            {content}
+            <h1 className='service-card-header'>{data?.length > 0 && data[params.id]?.serviceName}</h1>
+            <div className="detail-cards">
+                {content}
+            </div>
+
         </div>
     );
 };
